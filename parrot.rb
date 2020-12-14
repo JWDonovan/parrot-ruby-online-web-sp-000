cd7608109e78ad3c7b77ffb4961e9658715407b5
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 16:14:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = nil)
-  if phrase = nil
-    puts "#{phrase}"
-  else
-    puts "Squawk!"
-  end
+def parrot
+  puts "Squawk!"
 end
